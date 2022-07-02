196974f4f6c33194196477f76104dd4b871ab174
"Emmanuel Akin-Ademola" <"eakinademola@outlook.com">
2022-07-02 20:27:53 +0100

    initial commit

---
+++ contracts/simpleStorage.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+
+contract simpleStorage {
+     
+
+    uint256 favouriteNumber; 
+     mapping (string => uint256) public nameToFavouriteNumber;
+
+  struct People {
+       uint256 favouriteNumber;
+       string name;
+       }
+
+       People[] public people;
+   // This is a dynamic array
+
+    function  store(uint256 _favouriteNumber) public virtual {
+
+         favouriteNumber = _favouriteNumber;
+
+    }
+  function retrieve() public view returns(uint256){
+        return favouriteNumber;
+  } 
+        function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        
+            people.push(People( _favouriteNumber, _name));
+            nameToFavouriteNumber[_name] = _favouriteNumber;
+        }
+   }
+
